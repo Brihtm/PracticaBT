664a3690a869933ab240ea08a152d71e36af2ca6
Britany Toapanta <btoapantah@est.ups.edu.ec>
2026-05-04 16:11:33 -0500

    Trabajo en clase

---
+++ ejercicio.py
@@ -0,0 +1,23 @@
+#Crear un programa que pregunte tu nombre y edad, debe decir bienvenido con su nombre
+#Autor: Britany Toapanta
+#FECHA: 04/05/2026
+
+def main():
+    nombre = input("Buenos dias, ingresa tu nombre:")
+    print ("Hola",nombre, "bienvenido a tu programa favorito")
+    print(r"""          o  o   o  o
+         |\/ \^/ \/|
+         |,-------.|
+       ,-.(|)   (|),-.
+       \_*._ ' '_.* _/
+        /`-.`--' .-'\
+   ,--./    `---'    \,--.
+   \   |(  )     (  )|   /
+hjw \  | ||       || |  /
+`97  \ | /|\     /|\ | /
+     /  \-._     _,-/  \
+    //| \\  `---'  // |\\
+   /,-.,-.\       /,-.,-.\
+  o   o   o      o   o    o""")
+if __name__ == "__main__":
+    main()
